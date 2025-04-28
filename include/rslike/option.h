67f0814cb29f2c3d83e65922c6aeb8aee29b6445
Goolnn <Goolnn@hotmail.com>
2025-04-28 21:29:07 +0800

    feat: implement option type

---
+++ include/rslike/option.h
@@ -0,0 +1,45 @@
+#ifndef GOOLNN_RSLIKE_OPTION_H
+#define GOOLNN_RSLIKE_OPTION_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef enum {
+    Option_Some,
+    Option_None
+} Option;
+
+#define Option(T) Option ## _ ## T
+
+#define define_option(T) \
+typedef struct {         \
+    Option status;       \
+    union {              \
+        T some;          \
+    };                   \
+} Option ## _ ## T
+
+#define option_is_some(self) (self.status == Option_Some)
+
+#define option_is_some_and(T, self, x, f) (self.status == Option_Some); \
+{                                                                       \
+    if (self.status == Option_Some) {                                   \
+        T x = self.some;                                                \
+        f                                                               \
+    }                                                                   \
+}
+
+#define option_is_none(self) (self.status == Option_None)
+
+#define option_unwrap(T, self) (self.some);                 \
+{                                                           \
+    if (self.status == Option_None) {                       \
+        fprintf(                                            \
+            stderr,                                         \
+            "called `Option::unwrap()` on a `None` value\n" \
+        );                                                  \
+        abort();                                            \
+    }                                                       \
+}
+
+#endif
