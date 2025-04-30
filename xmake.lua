74154d11a820e8f2629af8be0ec2b53e378e7308
Goolnn <Goolnn@hotmail.com>
2025-04-30 20:45:11 +0800

    build: add xmacros dependency

--- xmake.lua
+++ xmake.lua
@@ -1,7 +1,19 @@
 add_rules("mode.debug", "mode.release")
 
+package("xmacros")
+    set_urls("https://github.com/Goolnn/xmacros.git")
+    on_install(function (package)
+        import("package.tools.xmake").install(package, {
+            "--mode=" .. (package:debug() and "debug" or "release")
+        })
+    end)
+package_end()
+
+add_requires("xmacros")
+
 target("rslike")
     set_kind("headeronly")
     set_languages("clatest")
     set_version("0.1.0")
+    add_packages("xmacros")
     add_headerfiles("include/(**.h)")
