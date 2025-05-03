a907682f7a14a422788de1adc2bac995ef91ece4
Goolnn <Goolnn@hotmail.com>
2025-05-03 15:09:19 +0800

    refactor: rewrite enum macros

--- include/rslike/enum.h
+++ include/rslike/enum.h
@@ -1,25 +1,22 @@
 #ifndef GOOLNN_RSLIKE_ENUM_H
 #define GOOLNN_RSLIKE_ENUM_H
 
-#include <xmacros/xmacros.h>
-
 #include "rslike/priv/enum.h"
 
 #define Variant(n, v) xmacros_combine(n, v)
+#define VariantName(n, v) xmacros_combine(n, variant_name)(v)
 
 #define define_enum(n, s)                                            \
 typedef enum {                                                       \
-    priv_variant(n, xmacros_unwrap s)                               \
+    priv_variants(n, xmacros_unpack(s))                              \
     xmacros_combine(n, Undefined)                                    \
 } n;                                                                 \
 static inline const char* xmacros_combine(n, variant_name)(n self) { \
     switch (self) {                                                  \
-        priv_variant_name(n, priv_unwrap s)                        \
+        priv_variant_names(n, xmacros_unpack(s))                     \
         default:                                                     \
-            return "Undefined";                                      \
+            return xmacros_to_string(xmacros_combine(n, Undefined)); \
     }                                                                \
 }
-
-#define VariantName(n, v) xmacros_combine(n, variant_name)(v)
 
 #endif
