31f4444961a84d7310a8de0ad24a4df10f6fc232
Goolnn <Goolnn@hotmail.com>
2025-05-15 17:29:55 +0800

    feat: enhance unwrap_result macro

--- include/rslike/result.h
+++ include/rslike/result.h
@@ -60,38 +60,10 @@ static inline Option(E) xmacros_combine(Result(T, E), err)( \
 }
 
 #define result_is_ok(self) (self.status == Result_Ok)
-
-#define result_is_ok_and(T, self, x, f) (self.status == Result_Ok); \
-{                                                                   \
-    if (self.status == Result_Ok) {                                 \
-        T x = self.ok;                                              \
-        f                                                           \
-    }                                                               \
-}
-
 #define result_is_err(self) (self.status == Result_Err)
-
-#define result_is_err_and(E, self, x, f) (self.status == Result_Err); \
-{                                                                     \
-    if (self.status == Result_Err) {                                  \
-        E x = self.err;                                               \
-        f                                                             \
-    }                                                                 \
-}
 
 #define result_ok(T, E, self) xmacros_combine(Result(T, E), ok)(self)
 #define result_err(T, E, self) xmacros_combine(Result(T, E), err)(self)
-
-#define result_unwrap(T, E, self) (self.ok);               \
-{                                                          \
-    if (self.status == Result_Err) {                       \
-        fprintf(                                           \
-            stderr,                                        \
-            "called `Result::unwrap()` on a `Err` value\n" \
-        );                                                 \
-        abort();                                           \
-    }                                                      \
-}
 
 #define if_let_ok(T, self, x, f) \
 if (self.status == Result_Ok) {  \
@@ -115,6 +87,22 @@ if (self.status == Result_Err) {  \
         E err = result.err;                              \
         handle_err;                                      \
     }                                                    \
+}
+
+#define unwrap_result(T, E, ident, self, ...)              \
+T ident;                                                   \
+{                                                          \
+    Result(T, E) result = self;                            \
+    if (result.status == Result_Ok) {                      \
+        ident = result.ok;                                 \
+    } else if (result.status == Result_Err) {              \
+        __VA_OPT__(xmacros_argn(0, __VA_ARGS__);)          \
+        fprintf(                                           \
+            stderr,                                        \
+            "called `Result::unwrap()` on a `Err` value\n" \
+        );                                                 \
+        abort();                                           \
+    }                                                      \
 }
 
 #endif
